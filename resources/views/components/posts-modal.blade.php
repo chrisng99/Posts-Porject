227acbd7672dfc6d4f0ab776cfe0c683e16ec683
chrisng99 <christopher.nwh99@gmail.com>
2023-01-10 18:31:26 +0800

    Added close button to post modals

--- resources/views/components/posts-modal.blade.php
+++ resources/views/components/posts-modal.blade.php
@@ -1,14 +1,19 @@
 <div>
     <!-- Page Heading -->
-    @if (isset($header))
-        <header class="bg-white shadow">
-            <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+    <header class="bg-white shadow">
+        <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8 flex justify-between">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                @if (isset($header))
                     {{ $header }}
-                </h2>
-            </div>
-        </header>
-    @endif
+                @endif
+            </h2>
+            <button wire:click="$emit('closeModal')">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x-circle">
+                    <circle cx="12" cy="12" r="10"></circle><line x1="15" y1="9" x2="9" y2="15"></line><line x1="9" y1="9" x2="15" y2="15"></line>
+                </svg>
+            </button>
+        </div>
+    </header>
 
     <div class="pb-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
