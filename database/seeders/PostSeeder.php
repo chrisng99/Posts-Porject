61e449a5af65669fa882cfc8c073daa84affdf1a
chrisng99 <christopher.nwh99@gmail.com>
2023-01-11 19:45:53 +0800

    Bug fix for PostSeeder.php

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -11,10 +11,16 @@ class PostSeeder extends Seeder
 {
     public function run(): void
     {
-        // Create 200 fake posts. Ensure at least one category and one user has been created before seeding posts
-        Post::factory(200)->create([
-            'category_id' => Category::pluck('id')->random(),
-            'user_id' => User::pluck('id')->random(),
-        ]);
+        // Create 200 fake posts.
+        // Ensure at least one category and one user has been created before seeding posts
+        $categories = Category::pluck('id');
+        $users = User::pluck('id');
+
+        foreach (range(1, 200) as $i) {
+            Post::factory()->create([
+                'category_id' => $categories->random(),
+                'user_id' => $users->random(),
+            ]);
+        }
     }
 }
